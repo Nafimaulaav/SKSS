a789d09128b829111e2088d277c928112917e4a2
Your Name <nafymaula7@gmail.com>
2025-09-30 20:11:43 +0700

    menambahkan favico, css dan mengubah beberapa gambar

--- tugas.php
+++ tugas.php
@@ -5,16 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Studi Kasus Client Side - Personal Website</title>
     <link rel="icon" type="image/x-icon" href="punyakulogo.png">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 </head>
 <body>
+    <div class="header">
+        <h1>Selamat Datang Di Personal Website Saya</h1>
+    </div>
+
     <div class="isi-konten">
-
-        <div class="header">
-            <h1>Selamat Datang Di Personal Website Saya</h1>
-        </div>
-
         <div class="foto-profil">
-            <img src="fotoprofil.jpg" alt="Foto Profil" width="200" height="250">
+            <img src="myfoto.jpg" alt="Foto Profil">
         </div>
 
         <div class="informasi-diri">
@@ -32,15 +33,15 @@
             <?php 
                 $sosmed = [
                     ["Faceboook", "https://www.facebook.com/mati.maula", "Facebook_Logo_Primary.png"],
-                    ["Instagram", "https://www.instagram.com/ishnapp", "Instagram_Glyph_Gradient.png"],
-                    ["Linkedin", "https://www.linkedin.com/in/nafi-maula", "Linkedin-logo.png"],
+                    ["Instagram", "https://www.instagram.com/ishnapp", "ig.png"],
+                    ["Linkedin", "https://www.linkedin.com/in/nafi-maula", "linkedine.png"],
                     ["Github", "https://github.com/Nafimaulaav", "github-mark.png"]
                 ];
 
                 foreach ($sosmed as $m) {
                     echo '<div class = "sosmed-item">
-                            <a href="' . $m[1] . '" target="blank">
-                                <img src="' . $m[2] . '" alt="' . $m[0] . '" width="50" height="50">
+                            <a href="' . $m[1] . '" target="blank" class="lingkaran">
+                                <img src="' . $m[2] . '" alt="' . $m[0] . '">
                             </a>    
                             <span>' . $m[0] . '</span>
                         </div>';
@@ -54,7 +55,7 @@
     <div class="footerd">
         <?php
             date_default_timezone_set("Asia/Jakarta");
-            echo "Waktu Sekarang : " . date("h:i:s d-m-Y")."<br>"; ;
+            echo "Waktu Sekarang : " . date("H:i:s d-m-Y")."<br>"; ;
             echo "Dibuat oleh Nafi Maulau Auditya Vindra";
         ?>
     </div>
